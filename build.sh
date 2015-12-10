8629796417e54aa553b9cba9ed2e769edc4c5de0
Derrek Young <derrek.young@appdynamics.com>
2015-12-10 12:06:42 -0500

    Make the zip extract into a top-level folder with name and version

--- build.sh
+++ build.sh
@@ -2,8 +2,27 @@
 
 VERSION="v0.7-BETA"
 
-DISTRIBUTABLE_NAME="ssl-cert-utils-"$VERSION".zip"
+DIST_DIR="./dist"
+DIST_TOP_FOLDER="appd-ssl-cert-utils-$VERSION"
+DISTRIBUTABLE_NAME="$DIST_TOP_FOLDER.zip"
 
-rm -R ./dist/
-mkdir ./dist
-zip ./dist/$DISTRIBUTABLE_NAME controller-ssl-certs-util.sh eum-ssl-certs-util.sh
+if [ -d "$DIST_DIR" ]; then
+  echo "Cleaning dist/ directory..."
+  rm -R $DIST_DIR
+fi
+
+if [ ! -d "$DIST_DIR" ]; then
+  echo "Making dist/ directory..."
+  mkdir $DIST_DIR
+fi
+
+# Create a top-level folder for when unzipping the archive
+mkdir $DIST_DIR/$DIST_TOP_FOLDER
+cp *-ssl-certs-util.sh $DIST_DIR/$DIST_TOP_FOLDER/
+
+echo "Creating the Zip file..."
+#zip $DIST_DIR/$DISTRIBUTABLE_NAME controller-ssl-certs-util.sh eum-ssl-certs-util.sh
+cd $DIST_DIR/
+zip -r $DISTRIBUTABLE_NAME $DIST_TOP_FOLDER/
+
+#unzip -l $DISTRIBUTABLE_NAME
